023b2f412e3a35c72e8c243c552e0781c9407289
Alex <Teplyakov.in@gmail.com>
2016-05-29 22:50:07 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+/**
+ * Created by diz on 27.05.2016.
+ */
+public class Main {
+
+    public int getSumm ( int a, int b)    {
+        return a + b;
+    }
+
+    public int getSub(int a, int b) {
+        return a - b;
+    }
+
+    public int getMult(int a, int b) {
+        return a * b;
+    }
+
+
+    public double getDivision(double a, double b)
+             throws
+            IllegalArgumentException
+    {
+        return (double) (a/b);
+    }
+
+}
---
+++ src/TestCalc.java
@@ -0,0 +1,45 @@
+/**
+ * Created by Alex on 28.05.2016.
+ */
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestCalc  {
+
+    private Main calc;
+
+    @BeforeTest
+    public void setUp(){
+        calc = new Main();
+    }
+
+    @Test(description = "First test of page constructor",enabled =true )
+    public void testCalcDivision(){
+
+        Assert.assertEquals(2.5,calc.getDivision(5,2),"деление проводится не правильно");
+    }
+    @Test(expectedExceptions = ArithmeticException.class)
+    public void divisionWithException() {
+        calc.getDivision(5,0);
+    }
+    @Test(description = "First test of page constructor",enabled =true )
+    public void testCalcMultt(){
+        int res= calc.getMult(5,5);
+        Assert.assertEquals(25, res, "умножение проводится не правильно");
+    }
+    @Test(description = "First test of page constructor",enabled =true )
+    public void testCalcSubt() {
+
+        int res = calc.getSub(5, 5);
+        Assert.assertEquals(0,res,"вычитание проводится не правильно");
+    }
+
+    @Test(description = "First test of page constructor",enabled =true )
+    public void testCalcSummt() {
+
+        int res= calc.getSumm(5,5);
+        Assert.assertEquals(10,res,"сложение проводится не правильно");
+
+    }
+}
